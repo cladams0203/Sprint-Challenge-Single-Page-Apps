57440c97e027381f1ee1e5729331ff5e7cce2c66
Chris Adams <cladams0203@gmail.com>
2019-09-14 14:17:22 -0400

    finished making home page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Route } from 'react-router-dom';
+import axios from 'axios';
+import styled from 'styled-components';
 
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
 import CharacterList from './components/CharacterList';
 import LocationsList from './components/LocationsList';
 import EpisodesList from './components/EpisodesList';
+// import HomeDisplay from './components/HomeDisplay'
 
+const HomeContainer = styled.div `
+  display:flex;
+  flex-direction: column;
+  align-items: center;
+
+`
 
 export default function App() {
+  
   return (
     <main>
       <Header />
       <TabNav />
+      
+      <Route exact path={`/`} component={HomeDisplay} />
       <Route path={`/characters`} component={CharacterList} />
       <Route path={`/locations`} component={LocationsList} />
       <Route path={`/episodes`} component={EpisodesList} />
 
     </main>
   );
+}
+function HomeDisplay() {
+
+  const [home, setHome] = useState({})
+  console.log(home)
+  useEffect(() => {
+    axios.get(`https://rickandmortyapi.com/api/character/`)
+    .then(response => {
+      setHome(response.data.results[0])
+    })
+    .catch(err => console.log(err))
+  }, [])
+  
+  
+  
+  return (
+      
+        <HomeContainer>
+          <img src={home.image} alt={home.name} />
+          <h2>{home.name}</h2>
+          <p>{`${home.species} - ${home.status} `}</p>
+      </HomeContainer>
+      
+      
+  );
+
 }
